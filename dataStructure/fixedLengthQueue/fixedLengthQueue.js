08c9e6ac3ad7202ef4834023e3abce62ff43ab49
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-21 07:49:23 +0800

    Add completed fixedLengthQueue kata

--- dataStructure/fixedLengthQueue/fixedLengthQueue.js
+++ dataStructure/fixedLengthQueue/fixedLengthQueue.js
@@ -1,9 +1,38 @@
 module.exports = class FixedLengthQueue {
-  constructor(length) {}
+  constructor(length) {
+    this.array = new Array(length);
+    this.incrementCount = 0;
+    this.decrementCount = 0;
+    // console.log(this.array[0]);
+  }
 
-  enqueue(item) {}
+  enqueue(item) {
+    if (this.array.length === this.incrementCount) {
+      throw new Error("max length reached");
+    }
 
-  dequeue() {}
+    this.array[this.incrementCount] = item;
+    this.incrementCount++;
+    console.log("after enqueue", this.array);
+  }
 
-  peek() {}
+  dequeue() {
+    const dequeueItem = this.array[this.decrementCount];
+    if (typeof dequeueItem !== "undefined") {
+      this.array[this.decrementCount] = undefined;
+      this.decrementCount++;
+    }
+
+    console.log("after dequeue", this.array);
+
+    return dequeueItem;
+  }
+
+  peek() {
+    return this.array[this.decrementCount];
+  }
+
+  getSize() {
+    return this.array.length;
+  }
 };
