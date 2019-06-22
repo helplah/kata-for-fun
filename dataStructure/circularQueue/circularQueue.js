61821862ee09a10e7d142a1eefe52e7406ff46c0
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-22 10:48:38 +0800

    Add completed circularQueue kata

--- dataStructure/circularQueue/circularQueue.js
+++ dataStructure/circularQueue/circularQueue.js
@@ -1,13 +1,45 @@
 module.exports = class CircularQueue {
   constructor(lengthOfArray) {
     this.array = new Array(lengthOfArray);
+    this.incrementCount = 0;
+    this.decrementCount = 0;
   }
 
-  enqueue(item) {}
+  enqueue(item) {
+    if (this.array.length === this.incrementCount) {
+      throw new Error("max length reached");
+    } else if (typeof item === "undefined") {
+      throw new Error("enqueue item cannot be undefined");
+    }
 
-  dequeue() {}
+    this.array[this.incrementCount] = item;
+    this.incrementCount++;
+  }
 
-  peek() {}
+  dequeue() {
+    let dequeueItem = this.array[this.decrementCount];
+
+    if (typeof dequeueItem !== "undefined") {
+      this.array[this.decrementCount] = undefined;
+      this.decrementCount++;
+      this.incrementCount--;
+    }
+
+    if (typeof dequeueItem === "undefined" && this.decrementCount > 0) {
+      this.decrementCount--;
+      dequeueItem = this.array[this.decrementCount];
+    }
+
+    if (typeof dequeueItem === "undefined") {
+      throw new Error("no item in queue");
+    }
+
+    return dequeueItem;
+  }
+
+  peek() {
+    return this.array[this.decrementCount];
+  }
 
   getSize() {
     return this.array.length;
--- dataStructure/circularQueue/circularQueue.test.js
+++ dataStructure/circularQueue/circularQueue.test.js
@@ -1,76 +1,76 @@
-const CircularQueue = require('./circularQueue');
+const CircularQueue = require("./circularQueue");
 
-describe('Circular Queue', () => {
-  it('should create an queue of size of length pass to constuctor', () => {
+describe("Circular Queue", () => {
+  it("should create an queue of size of length pass to constuctor", () => {
     const cq = new CircularQueue(3);
     expect(cq.getSize()).toEqual(3);
   });
 
-  it('should be able to enqueue and dequeue', () => {
+  it("should be able to enqueue and dequeue", () => {
     const cq = new CircularQueue(1);
-    cq.enqueue('apple');
-    expect(cq.dequeue()).toEqual('apple');
+    cq.enqueue("apple");
+    expect(cq.dequeue()).toEqual("apple");
     expect(cq.getSize()).toEqual(1);
   });
 
-  it('should not be able to add more if there is no empty slot in the queue', () => {
+  it("should not be able to add more if there is no empty slot in the queue", () => {
     const cq = new CircularQueue(1);
-    cq.enqueue('apple');
-    expect(() => cq.enqueue('banana')).toThrowError('max length reached');
+    cq.enqueue("apple");
+    expect(() => cq.enqueue("banana")).toThrowError("max length reached");
     expect(cq.getSize()).toEqual(1);
   });
 
-  it('should maintain order of enqueue and dequeue', () => {
+  it("should maintain order of enqueue and dequeue", () => {
     const cq = new CircularQueue(3);
-    cq.enqueue('apple');
-    cq.enqueue('banana');
-    cq.enqueue('citrus');
-    expect(cq.dequeue()).toEqual('apple');
-    expect(cq.dequeue()).toEqual('banana');
-    expect(cq.dequeue()).toEqual('citrus');
+    cq.enqueue("apple");
+    cq.enqueue("banana");
+    cq.enqueue("citrus");
+    expect(cq.dequeue()).toEqual("apple");
+    expect(cq.dequeue()).toEqual("banana");
+    expect(cq.dequeue()).toEqual("citrus");
     expect(cq.getSize()).toEqual(3);
   });
 
-  it('should be able to enqueue more than length of queue after some item dequeed', () => {
+  it("should be able to enqueue more than length of queue after some item dequeed", () => {
     const cq = new CircularQueue(1);
-    cq.enqueue('apple');
+    cq.enqueue("apple");
     cq.dequeue();
-    expect(() => cq.enqueue('banana')).not.toThrowError('max length reached');
-    expect(cq.dequeue()).toEqual('banana');
+    expect(() => cq.enqueue("banana")).not.toThrowError("max length reached");
+    expect(cq.dequeue()).toEqual("banana");
     expect(cq.getSize()).toEqual(1);
   });
 
-  it('should be able to peek on next item', () => {
+  it("should be able to peek on next item", () => {
     const cq = new CircularQueue(2);
-    cq.enqueue('apple');
-    expect(cq.peek()).toEqual('apple');
-    cq.enqueue('banana');
-    expect(cq.peek()).toEqual('apple');
+    cq.enqueue("apple");
+    expect(cq.peek()).toEqual("apple");
+    cq.enqueue("banana");
+    expect(cq.peek()).toEqual("apple");
     expect(cq.getSize()).toEqual(2);
   });
 
-  it('should be able to peek the correct item after dequeue', () => {
+  it("should be able to peek the correct item after dequeue", () => {
     const cq = new CircularQueue(3);
     expect(cq.peek()).toEqual(undefined);
-    expect(() => cq.dequeue()).toThrowError('no item in queue');
-    cq.enqueue('apple');
-    cq.enqueue('banana');
-    cq.enqueue('citrus');
-    expect(cq.peek()).toEqual('apple');
-    expect(cq.dequeue()).toEqual('apple');
-    expect(cq.peek()).toEqual('banana');
-    expect(cq.dequeue()).toEqual('banana');
-    expect(cq.peek()).toEqual('citrus');
-    expect(cq.dequeue()).toEqual('citrus');
-    expect(() => cq.dequeue()).toThrowError('no item in queue');
+    expect(() => cq.dequeue()).toThrowError("no item in queue");
+    cq.enqueue("apple");
+    cq.enqueue("banana");
+    cq.enqueue("citrus");
+    expect(cq.peek()).toEqual("apple");
+    expect(cq.dequeue()).toEqual("apple");
+    expect(cq.peek()).toEqual("banana");
+    expect(cq.dequeue()).toEqual("banana");
+    expect(cq.peek()).toEqual("citrus");
+    expect(cq.dequeue()).toEqual("citrus");
+    expect(() => cq.dequeue()).toThrowError("no item in queue");
     expect(cq.peek()).toEqual(undefined);
     expect(cq.getSize()).toEqual(3);
   });
 
-  it('cannot enqueue undefined', () => {
+  it("cannot enqueue undefined", () => {
     const cq = new CircularQueue(2);
     expect(() => cq.enqueue(undefined)).toThrowError(
-      'enqueue item cannot be undefined'
+      "enqueue item cannot be undefined"
     );
     expect(cq.getSize()).toEqual(2);
   });
