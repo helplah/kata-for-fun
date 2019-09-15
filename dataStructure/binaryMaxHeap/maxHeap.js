57082dd9cf0c8f912edb2e5ddde68a9d300650b6
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-15 16:46:49 +0800

    Add binaryMaxHeap

---
+++ dataStructure/binaryMaxHeap/maxHeap.js
@@ -0,0 +1,69 @@
+class MaxHeap {
+  constructor() {
+    this.priorityQueue = [];
+  }
+
+  getMax() {
+    return this.priorityQueue[0];
+  }
+
+  extractMax() {
+    const rootNode = this.priorityQueue[0];
+    this.priorityQueue[0] = this.priorityQueue[this.priorityQueue.length - 1];
+    this.priorityQueue.pop();
+
+    let parentIndex = 0;
+    let parent = this.priorityQueue[parentIndex];
+    let firstChildIndex = 2 * parentIndex + 1;
+    let firstChild = this.priorityQueue[firstChildIndex];
+    let secondChildIndex = 2 * parentIndex + 2;
+    let secondChild = this.priorityQueue[secondChildIndex];
+
+    let length = this.priorityQueue.length;
+    while (parentIndex < length) {
+      let largerChild = firstChild > secondChild ? firstChild : secondChild;
+      let largerChildIndex =
+        largerChild === firstChild ? firstChildIndex : secondChildIndex;
+
+      if (parent < largerChild) {
+        this.priorityQueue[parentIndex] = largerChild;
+        this.priorityQueue[largerChildIndex] = parent;
+        parentIndex = largerChildIndex;
+      }
+
+      firstChildIndex = 2 * parentIndex + 1;
+      secondChildIndex = 2 * parentIndex + 2;
+      parent = this.priorityQueue[parentIndex];
+      firstChild = this.priorityQueue[firstChildIndex];
+      secondChild = this.priorityQueue[secondChildIndex];
+      length--;
+    }
+    return rootNode;
+  }
+
+  insert(item) {
+    this.priorityQueue.push(item);
+    let itemIndex = this.priorityQueue.length - 1;
+
+    let itemParentIndex;
+    if (itemIndex > 0) {
+      itemParentIndex = Math.floor((itemIndex - 1) / 2);
+    } else {
+      itemParentIndex = Math.ceil((itemIndex - 1) / 2);
+    }
+
+    let parent = this.priorityQueue[itemParentIndex];
+    while (item > parent) {
+      this.priorityQueue[itemParentIndex] = item;
+      this.priorityQueue[itemIndex] = parent;
+
+      itemIndex = itemParentIndex;
+      itemParentIndex = Math.floor((itemIndex - 1) / 2);
+      parent = this.priorityQueue[itemParentIndex];
+    }
+  }
+}
+
+module.exports = {
+  MaxHeap
+};
---
+++ dataStructure/binaryMaxHeap/maxHeap.test.js
@@ -0,0 +1,72 @@
+const { MaxHeap } = require("./maxHeap");
+
+let maxHeap;
+beforeEach(() => {
+  maxHeap = new MaxHeap();
+});
+
+describe("MaxHeap", () => {
+  it("should construct a MaxHeap object", () => {
+    expect(maxHeap.priorityQueue).toEqual([]);
+  });
+
+  describe("insert", () => {
+    it("should insert an item", () => {
+      maxHeap.insert(100);
+      expect(maxHeap.priorityQueue[0]).toBe(100);
+    });
+
+    it("should insert multiple items", () => {
+      maxHeap.insert(100);
+      maxHeap.insert(10);
+      maxHeap.insert(20);
+      expect(maxHeap.priorityQueue[0]).toBe(100);
+      expect(maxHeap.priorityQueue[1]).toBe(10);
+      expect(maxHeap.priorityQueue[2]).toBe(20);
+    });
+
+    it("should recursively swap child with parent if child is greater than parent", () => {
+      maxHeap.insert(10);
+      maxHeap.insert(100);
+      maxHeap.insert(50);
+
+      maxHeap.insert(20);
+      maxHeap.insert(30);
+
+      maxHeap.insert(60);
+      maxHeap.insert(120);
+      expect(maxHeap.priorityQueue[0]).toBe(120);
+      expect(maxHeap.priorityQueue[1]).toBe(30);
+      expect(maxHeap.priorityQueue[2]).toBe(100);
+      expect(maxHeap.priorityQueue[3]).toBe(10);
+      expect(maxHeap.priorityQueue[4]).toBe(20);
+      expect(maxHeap.priorityQueue[5]).toBe(50);
+      expect(maxHeap.priorityQueue[6]).toBe(60);
+    });
+  });
+
+  it("getMax should return the root node", () => {
+    maxHeap.insert(100);
+    maxHeap.insert(300);
+    expect(maxHeap.getMax()).toBe(300);
+  });
+
+  it("extractMax should remove and return the root node", () => {
+    maxHeap.insert(10);
+    maxHeap.insert(100);
+    maxHeap.insert(50);
+
+    maxHeap.insert(20);
+    maxHeap.insert(30);
+
+    maxHeap.insert(60);
+    maxHeap.insert(120);
+    expect(maxHeap.extractMax()).toBe(120);
+    expect(maxHeap.priorityQueue[0]).toBe(100);
+    expect(maxHeap.priorityQueue[1]).toBe(30);
+    expect(maxHeap.priorityQueue[2]).toBe(60);
+    expect(maxHeap.priorityQueue[3]).toBe(10);
+    expect(maxHeap.priorityQueue[4]).toBe(20);
+    expect(maxHeap.priorityQueue[5]).toBe(50);
+  });
+});
