59990ad6a905991ffd515d7bf37f5e39e838356e
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-20 10:07:37 +0800

    Add mergeSort

---
+++ algorithms/mergeSort/mergeSort.js
@@ -0,0 +1,31 @@
+const merge = (left, right) => {
+  let resultArray = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArray.concat(left.slice(leftIndex), right.slice(rightIndex));
+};
+
+const mergeSort = array => {
+  if (array.length <= 1) {
+    return array;
+  }
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+module.exports = {
+  mergeSort
+};
